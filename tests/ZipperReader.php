69a9d1500df354682df17cf54052aa4da2a47dfa
mirko-pagliai <mirko.pagliai@gmail.com>
2022-03-11 17:45:57 +0100

    renamed package

--- tests/ZipperReader.php
+++ tests/ZipperReader.php
@@ -2,14 +2,14 @@
 declare(strict_types=1);
 
 /**
- * This file is part of cakephp-files-backup.
+ * This file is part of php-files-backup.
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright   Copyright (c) Mirko Pagliai
- * @link        https://github.com/mirko-pagliai/cakephp-files-backup
+ * @link        https://github.com/mirko-pagliai/php-files-backup
  * @license     https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace FilesBackup\Test;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,14 +2,14 @@
 declare(strict_types=1);
 
 /**
- * This file is part of cakephp-files-backup.
+ * This file is part of php-files-backup.
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright   Copyright (c) Mirko Pagliai
- * @link        https://github.com/mirko-pagliai/cakephp-files-backup
+ * @link        https://github.com/mirko-pagliai/php-files-backup
  * @license     https://opensource.org/licenses/mit-license.php MIT License
  */
 
@@ -23,7 +23,7 @@ define('TESTS', ROOT . 'tests' . DS);
 define('TEST_APP', TESTS . 'test_app' . DS);
 define('APP', TEST_APP . 'TestApp' . DS);
 define('APP_DIR', 'test_app');
-define('TMP', sys_get_temp_dir() . DS . 'cakephp-files-backup' . DS);
+define('TMP', sys_get_temp_dir() . DS . 'php-files-backup' . DS);
 define('CONFIG', APP . 'config' . DS);
 
 @mkdir(TMP, 0777, true);
