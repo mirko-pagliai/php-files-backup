ac3b9a4807a99cd03a0a8d0c1b43a1fa86951654
mirko-pagliai <mirko.pagliai@gmail.com>
2022-03-14 19:34:29 +0100

    added bootstrap

---
+++ config/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of php-files-backup.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/php-files-backup
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+if (!defined('ROOT')) {
+    define('ROOT', dirname(__DIR__) . DS);
+}
